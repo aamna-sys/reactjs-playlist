56e398b3dda06ab4b47b25749b56fe77c0da29dc
aamna-sys <aamna.sayyed@systemsltd.com>
2021-11-26 15:18:04 +0500

    Delete logo.svg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Props from "./components/Props/Props";
 
 function App() {
-  return (
-    <div className="App">
-      <Props />
-    </div>
-  );
+  return <div className="App">{/* <Props /> */}</div>;
 }
 
 export default App;
